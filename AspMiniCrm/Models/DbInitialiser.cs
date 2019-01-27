3e04f10f3b74dfcefe910e7a5a10fd26a1bfe665
philsmith86 <phil@supertapp-interactive.com>
2019-01-27 09:32:02 +0000

    Created database seeder

---
+++ AspMiniCrm/Models/DbInitialiser.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspMiniCrm.Models
+{
+    public class DbInitialiser
+    {
+        public static void Seed(CrmDbContext context)
+        {
+            if (!context.Companies.Any())
+            {
+                context.AddRange
+                (
+                    new Company
+                    {
+                        Name = "Google",
+                        Email = "google@email.com",
+                        Web = "https://www.google.com",
+                    },
+                    new Company
+                    {
+                        Name = "Yahoo",
+                        Email = "yahoo@email.com",
+                        Web = "https://www.yahoo.com",
+                    },
+                     new Company
+                     {
+                         Name = "Microsoft",
+                         Email = "microsoft@email.com",
+                         Web = "https://www.microsoft.com",
+                     },
+                     new Company
+                     {
+                         Name = "Facebook",
+                         Email = "facebook@email.com",
+                         Web = "https://www.facebook.com",
+                     },
+                     new Company
+                     {
+                         Name = "SpaceX",
+                         Email = "spacex@email.com",
+                         Web = "https://www.space-x.com",
+                     });
+            }
+
+            context.SaveChanges();
+
+            if (!context.Employees.Any())
+            {
+                context.AddRange(
+                    new Employee
+                    {
+                        FirstName = "John",
+                        LastName = "Adams",
+                        Telephone = "01236875212",
+                        Email = "john.adams@email.com",
+                        CompanyId = 1
+                    },
+                    new Employee
+                    {
+                        FirstName = "Peter",
+                        LastName = "Jinks",
+                        Telephone = "01238675212",
+                        Email = "pete.jinks@email.com",
+                        CompanyId = 2
+                    },
+                    new Employee
+                    {
+                        FirstName = "Lindsey",
+                        LastName = "Smith",
+                        Telephone = "01654875212",
+                        Email = "lindseys@email.com",
+                        CompanyId = 3
+                    },
+                    new Employee
+                    {
+                        FirstName = "Sarah",
+                        LastName = "Kineton",
+                        Telephone = "01565875230",
+                        Email = "sarahk@email.com",
+                        CompanyId = 4
+                    },
+                    new Employee
+                    {
+                        FirstName = "Lisa",
+                        LastName = "James",
+                        Telephone = "01897875212",
+                        Email = "thebiglj@email.com",
+                        CompanyId = 5
+                    }
+                );
+            }
+            
+            context.SaveChanges();
+        }
+    }
+}
--- AspMiniCrm/Program.cs
+++ AspMiniCrm/Program.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using AspMiniCrm.Models;
 
 namespace AspMiniCrm
 {
@@ -14,7 +16,24 @@ namespace AspMiniCrm
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<CrmDbContext>();
+                    DbInitialiser.Seed(context);
+                }
+                catch (Exception)
+                {
+                    //we could log this in a real-world situation
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
